fa5901d27c873dfbaed8a2d7b05f88d3544fd013
Rob Bultman <rob@firstbuild.com>
2018-04-23 09:44:55 -0400

    Initial add.

---
+++ extras/Testing/tdd-tests.mk
@@ -0,0 +1,45 @@
+#---------
+#
+# CppUTest Examples Makefile
+#
+#----------
+
+#Set this to @ to keep the makefile quiet
+ifndef SILENCE
+	SILENCE = @
+endif
+
+CPPUTEST_CXXFLAGS += -Wno-old-style-cast
+
+#--- Inputs ----#
+COMPONENT_NAME = RingBufferT
+CPPUTEST_HOME = extras/Testing/cpputest
+
+CPPUTEST_USE_GCOV = Y
+
+CPPUTEST_USE_EXTENSIONS = Y
+CPP_PLATFORM = Gcc
+
+CPPUTEST_WARNINGFLAGS += -Wno-unknown-warning-option
+
+# This line is overriding the default new macros.  This is helpful
+# when using std library includes like <list> and other containers
+# so that memory leak detection does not conflict with stl.
+#CPPUTEST_MEMLEAK_DETECTOR_NEW_MACRO_FILE = -include ApplicationLib/ExamplesNewOverrides.h
+#SRC_DIRS = \
+
+SRC_FILES = \
+	    src/RingBufT.cpp \
+	    
+TEST_SRC_DIRS = \
+	extras/Testing/ \
+	extras/Testing/tests \
+
+MOCKS_SRC_DIRS = \
+
+INCLUDE_DIRS =\
+	      src/ \
+
+include $(CPPUTEST_HOME)/build/MakefileWorker.mk
+
+
---
+++ extras/run-tests.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+make -f extras/Testing/tdd-tests.mk
+
